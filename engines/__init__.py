4615bf1b10444411d41be914ee86e0dedb82dcb4
cklxx <q1293822641@gmail.com>
2026-06-18 16:33:13 +0800

    split qwen3 greedy engine

--- engines/__init__.py
+++ engines/__init__.py
@@ -1,5 +1,33 @@
 """Tiny experimental engine loops."""
 
+from .qwen3_greedy_engine import (
+    ABLATION_VARIANTS,
+    BEST_PRACTICE_MODES,
+    KernelSetQwen3BestPracticePath,
+    KernelSetQwen3ConfigurablePath,
+    KernelSetQwen3FullPath,
+    KernelStats,
+    TORCH_MANUAL_MODES,
+    kernel_coverage_for_modes,
+    make_rope_cache,
+    merge_modes,
+    rms_eps,
+)
 from .tiny_engine import DiffusionResult, GreedyResult, TinyEngine
 
-__all__ = ["DiffusionResult", "GreedyResult", "TinyEngine"]
+__all__ = [
+    "ABLATION_VARIANTS",
+    "BEST_PRACTICE_MODES",
+    "DiffusionResult",
+    "GreedyResult",
+    "KernelSetQwen3BestPracticePath",
+    "KernelSetQwen3ConfigurablePath",
+    "KernelSetQwen3FullPath",
+    "KernelStats",
+    "TORCH_MANUAL_MODES",
+    "TinyEngine",
+    "kernel_coverage_for_modes",
+    "make_rope_cache",
+    "merge_modes",
+    "rms_eps",
+]
